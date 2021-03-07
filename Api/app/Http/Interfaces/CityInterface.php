623c502b2c83d7fea5438acebc311b32f95d1cb0
Diego Cruz <dacruzt@outlook.com>
2021-03-06 20:58:43 -0500

    Api Final de Proyecto

---
+++ Api/app/Http/Interfaces/CityInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Interfaces;
+
+
+interface CityInterface
+{
+    /**
+     * Undocumented function
+     *
+     * @param array $request
+     */
+    public function index(array $request);
+
+}
